a642ca3b4739485ce9fd86bbb115f17b15dadd41
40456381 <40456381@napier.ac.uk>
2020-02-16 10:20:56 +0000

    updated select

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -93,9 +93,14 @@ public class App
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
+                    "SELECT e.first_name, e.last_name, t.title, s.salary, d.dept_name "
+                            + "FROM employees e "
+                            + "JOIN titles t on e.emp_no = t.emp_no "
+                            + "JOIN salaries s on e.emp_no = s.emp_no "
+                            + "JOIN dept_emp de on e.emp_no = de.emp_no "
+                            + "JOIN departments d on d.dept_no = de.dept_no "
+                            + "WHERE s.to_date = '9999-01-01' "
+                            + "AND e.emp_no = " + ID;
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
